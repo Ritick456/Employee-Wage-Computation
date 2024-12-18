90bc61a9cf3ce8d7d280c1f923f1145ab66d9354
Ritick456 <ritickkr937@gmail.com>
2024-12-18 15:28:12 +0530

    Made some changes in employee-wage file

--- employee_wage.cpp
+++ employee_wage.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
+#include<cstdlib>
+#include<time.h>
 using namespace std;
 
 int main(){
 
     cout << "Welcome to Employee-Wage-Computation" << endl;
+
+     srand(time(0));
+
+        int randombit = rand() % 2; 
+        
+        if(randombit == 0){
+            cout << "Employee is Absent" << endl;;
+        }
+        else{
+            cout << "Employee is Present";
+        }
+
     return 0;
 }
