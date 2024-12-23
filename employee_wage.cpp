ff877c751c321377c3a0d1da875e8e050ae9cf74
Ritick456 <ritickkr937@gmail.com>
2024-12-23 14:31:04 +0530

    Calculated daily wage of part time employee

--- employee_wage.cpp
+++ employee_wage.cpp
@@ -6,7 +6,10 @@ using namespace std;
 
 void wagecalculation(int employeetype){
     if(employeetype == 1){
-        cout << "Employee is Full Time and his wage is " << 8 * 20 << " " << endl;
+        cout << "Employee is Full Time and his wage is "<< " " << 8 * 20 << endl;
+    }
+    if(employeetype == 0){
+        cout << "Employee is Part Time and his wage is" << " " << 4 * 20 << endl;
     }
 }
 
