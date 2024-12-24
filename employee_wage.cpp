9b06cacdaef2db72de1feb339fd95c87d85d4acf
Ritick456 <ritickkr937@gmail.com>
2024-12-24 09:41:49 +0530

    Calculated MOnthly wages of full time and part time employee

--- employee_wage.cpp
+++ employee_wage.cpp
@@ -4,14 +4,17 @@
 using namespace std;
 
 
-void wagecalculation(int employeetype){
+void wagecalculation(int employeetype, int &fulltimeempmonthlysalary, int &parttimeempmonthlysalary){
 
     switch(employeetype){
         case 1:
-            cout << "Employee is Full Time and his wage is "<< " " << 8 * 20 << endl;
+            //cout << "Employee is Full Time and his wage is "<< " " << 8 * 20 << endl;
+            fulltimeempmonthlysalary += 8*20;
             break;
+
         default :
-            cout << "Employee is Part Time and his wage is" << " " << 4 * 20 << endl;
+            //cout << "Employee is Part Time and his wage is" << " " << 4 * 20 << endl;
+            parttimeempmonthlysalary += 4*20;
 
     }
 
@@ -21,23 +24,30 @@ int main(){
 
     cout << "Welcome to Employee-Wage-Computation" << endl;
 
-     srand(time(0));
+    int parttimeempmonthlysalary = 0;
+    int fulltimeempmonthlysalary = 0;
+    srand(time(0));
 
-        int randombit = rand() % 2; 
+    for(int i=1; i<=20; i++){
+
+        int randombit = rand() % 2;
         
         switch(randombit){
 
             case 0:
-                cout << "Employee is Absent" << endl;
+                cout << "Day" << " " << i << " " << "Employee is Absent" << endl;
                 break;
             default :
-                cout << "Employee is Present" << endl;
+                cout << "Day" << " " << i << " " <<"Employee is Present" << endl;
 
                 int employeetype = rand() % 2;
-                wagecalculation(employeetype);
+                wagecalculation(employeetype, parttimeempmonthlysalary, fulltimeempmonthlysalary);
 
-                
         }
+    }
+
+    cout << "Monthly salary of full time employee is" << " "  << fulltimeempmonthlysalary << endl;
+    cout << "Monthly salary of part time employee is" << " " << parttimeempmonthlysalary << " " << endl;
 
     return 0;
 }
